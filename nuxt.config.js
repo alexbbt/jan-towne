dd65b72b42505645c45f3cf1e9c12b6206bb9ab3
Alexander Bell-Towne <alexbbt@squareup.com>
2022-04-22 00:38:23 +0100

    no ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vue-progressive-image.js', mode: 'client' }
+    { src: '~/plugins/vue-progressive-image.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
