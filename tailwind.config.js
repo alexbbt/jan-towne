b21103cb8ca7e950bf396ad0f302c2b4c90f0cd6
Alexander Bell-Towne <alexbbt@squareup.com>
2022-04-22 02:31:42 +0100

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px'
+      }
+    },
+    extend: {}
+  },
+  plugins: []
+}
